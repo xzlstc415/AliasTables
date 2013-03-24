0490facc41596c79c2f2be1f9364ae7c5a8b3e7c
pjs <pjs@alum.mit.edu>
2013-03-23 17:38:55 -0700

    Modified test_alias to generate 1000000 values and report 95% ranges
    Signed-off-by: pjs <pjs@alum.mit.edu>

--- test/test_alias.rb
+++ test/test_alias.rb
@@ -2,7 +2,7 @@
 
 require './lib/alias.rb'
 
-nvars = 10
+nvars = 1000000
 begin
   at = AliasTable.new(["yes", "no"], [0.3, 0.3, 0.4])
   nvars.times {print at.generate, "\n"}
@@ -13,15 +13,25 @@ Dir["test/infile.*"].each do |f_name|
   x = []
   probs = []
   f = File.open(f_name, "r")
+  counts = {}
+  expected_counts = {}
   while line = f.gets do
-    inputs = line.strip.split(/[\s,;:]+/).map{|x| x.to_f}
+    inputs = line.strip.split(/[\s,;:]+/)
     x << inputs[0]
-    probs << inputs[1]
+    counts[inputs[0]] = 0
+    probs << inputs[1].to_f
+    n_hat = probs[-1] * nvars
+    half_width = 2.5 * Math::sqrt(n_hat * (1.0 - probs[-1])) if n_hat > 0
+    expected_counts[inputs[0]] = "%d +/- %d" % [n_hat, half_width]
   end
   f.close
   begin
     at = AliasTable.new(x, probs)
-    nvars.times {print at.generate, "\n"}
+    nvars.times {counts[at.generate] += 1}
+    puts "\nAll four values should be in range 95\% of the time:"
+    counts.each_key do |k|
+      printf "%s: Expected %s, got %d\n", k, expected_counts[k], counts[k]
+    end
   rescue Exception => e
     puts e.message
   end
