878a39cfd65ce7532165cc42a1d921b201839200
pjs <pjs@alum.mit.edu>
2013-03-23 18:41:53 -0700

    Added documentation
    Signed-off-by: pjs <pjs@alum.mit.edu>

--- lib/alias.rb
+++ lib/alias.rb
@@ -2,8 +2,22 @@
 
 require 'skewheap'
 
+# Generate values from a categorical distribution in constant
+# time, regardless of the number of categories.  This clever algorithm
+# uses conditional probability to construct a table comprised of columns
+# which have a primary value and an alias.  Generating a value consists
+# of picking any column (with equal probabilities), and then picking
+# between the primary and the alias based on appropriate conditional
+# probabilities.
+#
 class AliasTable
-  
+
+  # Construct an alias table from a set of values and their associated
+  # probabilities.  Values and their probabilities must be synchronized,
+  # i.e., they must be arrays of the same length.  Values can be
+  # anything, but the probabilities must be positive numbers that
+  # sum to one.
+  # 
   def initialize(values, p_values)
     if values.length != p_values.length
       raise "Args to AliasTable must be vectors of the same length."
@@ -12,7 +26,7 @@ class AliasTable
     if p_values.reduce(:+).not_close_enough(1.0)
       raise "p_values must sum to 1.0"
     end
-    @values = values
+    @values = values.clone
     @p_values = p_values.clone
     @alias = Array.new(values.length)
     @p_primary = Array.new(values.length, 1.0)
@@ -29,7 +43,11 @@ class AliasTable
       classify(surplus_column)
     end
   end
-  
+
+  # Generate a random outcome.  This process requires constant time,
+  # but is not an inversion since two uniforms are used for each value
+  # that gets generated.
+  # 
   def generate
     column = rand(@values.length)
     rand < @p_primary[column] ? @values[column] : @alias[column]
@@ -49,6 +67,8 @@ class AliasTable
 end
 
 class Numeric
+  # Expand class Numeric to detect whether two values are within a
+  # tolerance of 10^-15 of each other.
   def not_close_enough(n)
     ((self - n) / self).abs > 1E-15
   end
